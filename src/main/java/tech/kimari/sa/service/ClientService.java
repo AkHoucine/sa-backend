8d4f32ac90bed41473ef16be7d1411bd55c13cb4
Kimari <akram.houcine@gmail.com>
2024-03-19 13:45:46 +0100

    Add DTO client mapper

--- src/main/java/tech/kimari/sa/service/ClientService.java
+++ src/main/java/tech/kimari/sa/service/ClientService.java
@@ -8,7 +8,6 @@ import tech.kimari.sa.mapper.ClientDTOMapper;
 import tech.kimari.sa.repository.ClientRepository;
 
 
-import java.util.List;
 import java.util.Optional;
 import java.util.stream.Stream;
 
